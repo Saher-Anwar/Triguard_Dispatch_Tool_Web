2ca071ce08ca0692f4c03a03c4355f8584551b2f
Saher <saher.ziauddin@dal.ca>
2025-10-21 18:03:40 -0300

    Setup shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
-
   return (
-    <p> Nothing </p>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button size={'icon'}>Click me</Button>
+    </div>
   )
 }
 
